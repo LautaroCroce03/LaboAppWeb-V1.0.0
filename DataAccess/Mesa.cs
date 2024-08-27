b49b2287cc94c976ecedc541eeebb17ea97ece51
Bostero <benedicto_paco_o@hotmail.com>
2024-08-27 09:33:23 -0300

    base

---
+++ DataAccess/Mesa.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LaboAppWebV1._0._0.DataAccess
+{
+    public class Mesa
+    {
+        private readonly LaboAppWebV1Context _laboAppWebV1Context;
+
+        public async Task<Int32> AgregarAsync(Models.Mesa mesa) 
+        {
+
+            try
+            {
+                await _laboAppWebV1Context.AddAsync(mesa);
+                await _laboAppWebV1Context.SaveChangesAsync();
+
+                return mesa.IdMesa;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<List<Models.Mesa>> Listado()
+        {
+            try
+            {
+                var result = await _laboAppWebV1Context.Mesas.ToListAsync();
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
